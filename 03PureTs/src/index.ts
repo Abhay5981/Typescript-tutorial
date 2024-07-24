6287e632cd86bdc6ae4706fed7090b6342330cc5
Abhay5981 <abhaykum78@gmail.com>
2024-07-24 10:23:01 +0530

    first commit

--- 03PureTs/src/index.ts
+++ 03PureTs/src/index.ts
@@ -1,2 +1,44 @@
-console.log("i am still here")
-console.log("i am still amezing")
+// class User {
+//   public email: string; // Accessible from anywhere.
+//   private name: string; //  if we  dont want  to access it then use private and we can access it within the class
+//   readonly city: string = "jaipur"; // when we use readonly we are allow to acces it but not set it
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// we can writ it with different way 
+
+
+class User {
+ 
+  private _courseCount:number = 1
+
+  readonly city: string = "jaipur"; // when we use readonly we are allow to acces it but not set it
+  constructor(
+    public email: string,  
+    public name: string
+  ) {
+   //getter
+  }
+  get  getAppleEmail(): String{
+    return  `apple ${this.email}`
+  }
+
+  get courseCount():number {
+    return this._courseCount
+  }
+  // setter 
+   set courseCount(courseNum){
+    if (courseNum<=1) {
+      throw new Error("courseCount should be more then 1")
+      
+    }
+    this._courseCount= courseNum
+   }
+}
+
+const abhay = new User("h@gmail.com", "abhay");
+//   abhay.city = "bengaluru";
+// abhay.name
